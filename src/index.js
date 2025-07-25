d64a03de0f9c7c85f2bf1b138c4cb5b070c175d8
Thais Sanches <thaisres@live.com>
2025-07-25 15:46:32 -0300

    feat: cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 //tem que importar as rotas aqui
 import produtosRouter from "./routers/produtosRouter.js";
 import usuariosRouter from "./routers/usuariosRouter.js";
 
 const server = express();
+server.use(cors({origin: "*"})); //qualquer origem pode acessar nossos endpoints da api
 server.use(express.json()); // Para o Express entender JSON no body. Todas as requisições ele converte pra JSON
 
 const port = process.env.PORT || 3000;
